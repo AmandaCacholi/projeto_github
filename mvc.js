218e15f3b9e0ed679e20b5e8e8fe9f9190bef167
AmandaCacholi <amanda.cacholi@outlook.com>
2020-11-14 15:39:31 -0300

    Primeira parte do projeto GitHub

---
+++ mvc.js
@@ -0,0 +1,105 @@
+    class Model{
+
+        constructor() {
+            
+            this._name = "";
+            this._repository = inputUser.value;
+            this._url = "";
+            this._login = "";
+            this._image = "";
+        }
+
+        buscaUser(){
+
+            let request = new XMLHttpRequest();
+            
+            request.addEventListener( "load", () => {
+                if ( request.status == 200 ) {
+                    let dados = this._processaResponse( request.responseText );
+                    this._atualiza( dados );
+                }
+                
+                try{
+                    if (request.status != 200){
+                        throw new Error("Nome de usuário inválido")
+
+                        
+                    }
+                }
+                catch(error){
+                    alert("Digite um nome de usuário válido.")
+                    document.location.reload();
+                }
+            })
+
+            request.open( "GET", "https://api.github.com/users/"+ this._repository +"/repos", false );
+            request.send();
+        }
+
+        _processaResponse( responseString ){
+
+            let response = JSON.parse( responseString );
+            return response;
+        }
+
+        _atualiza( dados ){
+
+            let divJs = document.querySelector(".js");
+
+            for (let i of dados){
+                let aJs = document.createElement("a");
+                divJs.appendChild(aJs);
+                aJs.innerHTML = `<p>Repositório: <a href="${i.html_url}">${i.name}</a><br>Linguagem: ${i.language}</p>`;
+            }
+
+            this._login = dados[0].owner.login;
+            this._image = dados[0].owner.avatar_url;
+
+        }
+
+        getLogin () {
+            return this._login;
+        }
+        getImage () {
+            return this._image;
+        }
+
+    }
+
+    class View{
+        constructor() {}
+
+        render( modelo ){
+
+            let userName = document.querySelector(".user");
+            let divImg = document.querySelector(".image");
+
+            userName.innerHTML = modelo.getLogin();
+            divImg.innerHTML = `<img src=${modelo.getImage()}>`;
+        }
+    }
+
+    class Controller{
+        constructor() {}
+
+        adicionaUser(){
+            let addUser = new Model();
+            addUser.buscaUser();
+
+            let view = new View();
+            view.render( addUser );
+
+            let removeForm = inputUser.remove();
+            
+            busca.textContent = "Pesquisar novamente";
+            busca.onclick = function(){
+                document.location.reload(true);
+            }
+
+        }
+    }
+    
+    let controller = new Controller();
+    
+    busca.addEventListener( "click", controller.adicionaUser );
+    
